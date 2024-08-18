56f7b0fd08449a64d88cebd514b4bbfb4a2fff4a
Prasad Poul <prasadpoul.2024.com>
2024-08-18 11:37:53 +0530

    Sixth Assignment

---
+++ PizzaShop/Pizza.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+public class Pizza
+{
+    // Private instance variables
+    private string pizzaSize;
+    private int cheeseToppings;
+    private int pepperoniToppings;
+    private int hamToppings;
+
+    // Constructor that sets all instance variables
+    public Pizza(string size, int cheese, int pepperoni, int ham)
+    {
+        pizzaSize = size;
+        cheeseToppings = cheese;
+        pepperoniToppings = pepperoni;
+        hamToppings = ham;
+    }
+
+    public string PizzaSize
+    {
+        get { return pizzaSize; }
+        set { pizzaSize = value; }
+    }
+
+    public int CheeseToppings
+    {
+        get { return cheeseToppings; }
+        set { cheeseToppings = value; }
+    }
+
+    public int PepperoniToppings
+    {
+        get { return pepperoniToppings; }
+        set { pepperoniToppings = value; }
+    }
+
+    public int HamToppings
+    {
+        get { return hamToppings; }
+        set { hamToppings = value; }
+    }
+
+    // Method to calculate the cost of the pizza
+    public double CalcCost()
+    {
+        double baseCost = 0;
+
+        switch (pizzaSize.ToLower())
+        {
+            case "small":
+                baseCost = 10;
+                break;
+            case "medium":
+                baseCost = 12;
+                break;
+            case "large":
+                baseCost = 14;
+                break;
+            default:
+                throw new ArgumentException("Invalid pizza size");
+        }
+
+        // Calculate total cost
+        double totalCost = baseCost + 2 * (cheeseToppings + pepperoniToppings + hamToppings);
+        return totalCost;
+    }
+
+    // Method to get the description of the pizza
+    public string GetDescription()
+    {
+        return $"Size: {pizzaSize}, Cheese Toppings: {cheeseToppings}, Pepperoni Toppings: {pepperoniToppings}, Ham Toppings: {hamToppings}, Cost: ${CalcCost()}";
+    }
+}
+
+
+
---
+++ PizzaShop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        // list to store pizza records
+        List<Pizza> pizzas = new List<Pizza>();
+
+        // Add some pizza records
+        pizzas.Add(new Pizza("small", 1, 1, 0));
+        pizzas.Add(new Pizza("medium", 2, 0, 1));
+        pizzas.Add(new Pizza("large", 3, 2, 1));
+
+        // Display the records
+        foreach (Pizza pizza in pizzas)
+        {
+            Console.WriteLine(pizza.GetDescription());
+        }
+    }
+}
